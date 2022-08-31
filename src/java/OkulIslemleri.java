c6e9371c86d4af2ab0b1897bc1adfc2f5b36d19b
Fatma Tasdemir <f.tasdemir571@gmail.com>
2022-08-31 11:20:54 +0200

    OKUL

---
+++ src/java/OkulIslemleri.java
@@ -0,0 +1,66 @@
+package java;
+
+public class OkulIslemleri {
+
+    public static void main(String[] args) {
+
+        /* Ögrenci EKLEME  ======>>FATMA TASDEMIR
+        private String ogrenciNo;
+        private String sinif;
+
+    public Ogrenci(String adSoyad, String kimlikNo, int yas, String ogrenciNo, String sinif) {
+            super(adSoyad, kimlikNo, yas);
+            this.ogrenciNo = ogrenciNo;
+            this.sinif = sinif;
+
+
+        }
+    }
+    */
+
+
+        /* ÖGRETMEN EKLEME ======>>> MEHMET AKIN
+     public Ogretmen(){
+        }
+
+    public Ogretmen(String adSoyad, String kimlikNo, int yas, String bolum, String sicilNo) {
+            super(adSoyad, kimlikNo, yas);
+            this.bolum = bolum;
+            this.sicilNo = sicilNo;
+
+
+        }
+
+
+*/
+
+
+
+
+
+        /*   PERSONEL EKLEME =====>> MEHMET B.AKTAS
+
+
+
+          public Kisi(String adSoyad, String kimlikNo, int yas) {
+            this.adSoyad = adSoyad;
+            this.kimlikNo = kimlikNo;
+            this.yas = yas;
+        }
+
+
+
+
+*/
+
+
+
+
+    }
+
+}
+
+
+
+
+
